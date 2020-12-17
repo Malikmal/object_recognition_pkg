d97b97590a16012be1ee47edec656764a03a81d4
Ikmalil Birri <malikmal98@gmail.com>
2020-12-17 15:51:55 +0700

    progress extrac_feature using vfh

---
+++ src/extract_feature.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <vector>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/features/vfh.h>
+#include <pcl/features/normal_3d.h>
+#include<pcl/visualization/histogram_visualizer.h>
+
+int main (int argc, char** argv)
+{
+  std::string fileName = argv[1];
+  std::cout << "Reading " << fileName << std::endl;
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+  if (pcl::io::loadPCDFile<pcl::PointXYZ> (fileName, *cloud) == -1) //* load the file
+  {
+    PCL_ERROR ("Couldn't read file");
+    return (-1);
+  }
+
+  std::cout << "Loaded " << cloud->points.size() << " points." << std::endl;
+
+  // Compute the normals
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normalEstimation;
+  normalEstimation.setInputCloud (cloud);
+
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+  normalEstimation.setSearchMethod (tree);
+
+  pcl::PointCloud<pcl::Normal>::Ptr cloudWithNormals (new pcl::PointCloud<pcl::Normal>);
+
+  normalEstimation.setRadiusSearch (0.03);
+
+  normalEstimation.compute (*cloudWithNormals);
+
+  std::cout << "Computed " << cloudWithNormals->points.size() << " normals." << std::endl;
+  
+  // Setup the feature computation
+  typedef pcl::VFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::VFHSignature308> VFHEstimationType;
+  VFHEstimationType vfhEstimation;
+
+  // Provide the original point cloud (without normals)
+  vfhEstimation.setInputCloud (cloud);
+
+  // Provide the point cloud with normals
+  vfhEstimation.setInputNormals(cloudWithNormals);
+
+  // Use the same KdTree from the normal estimation
+  vfhEstimation.setSearchMethod (tree);
+
+  //vfhEstimation.setRadiusSearch (0.2); // With this, error: "Both radius (.2) and K (1) defined! Set one of them to zero first and then re-run compute()"
+
+  // Actually compute the VFH features
+  pcl::PointCloud<pcl::VFHSignature308>::Ptr vfhFeatures(new pcl::PointCloud<pcl::VFHSignature308>);
+  vfhEstimation.compute (*vfhFeatures);
+
+  std::cout << "output points.size (): " << vfhFeatures->points.size () << std::endl; // This outputs 1 - should be 397!
+
+  // Display and retrieve the shape context descriptor vector for the 0th point.
+//   pcl::VFHSignature308 descriptor = vfhFeatures->points[0].;
+//   VFHEstimationType::PointCloudOut::PointType descriptor2 = vfhFeatures->points[0];
+//   std::cout << vfhFeatures->points[0] << std::endl;
+//   std::cout << vfhFeatures->points[0].histogram[30] << std::endl;
+
+
+    std::vector<float> VFHValue;
+    for(auto it : vfhFeatures->points[0].histogram)
+    {
+        // std::cout << it << std::endl;
+        VFHValue.push_back(it);
+    }
+
+    for(auto it : VFHValue)
+    {
+        std::cout << it << std::endl;
+        // VFHValue.push_back(it);
+    }
+
+    
+
+
+//   // //ascii
+//   std::string extension (".pcd");
+
+// //   std::stringstream ss;
+// //   ss << "src/object_recognition_pkg/output_vfh/" << fileName << ".pcd";
+//   transform (fileName.begin (), fileName.end (), fileName.begin (), (int(*)(int))tolower);
+//   std::cout << fileName.str() << std::endl;
+// //   pcl::io::savePCDFile(ss.str(), *vfhFeatures);
+
+// //   std::string ss = "src/object_recognition_pkg/ouput_vfh/";
+// //   ss += fileName;
+// //   std::cout << ss << std::endl;
+// //   pcl::io::savePCDFile(ss, *vfhFeatures);
+// //   pcl::PCDWriter writer;
+// //     writer.write(ss.str (), *vfhFeatures, false); //*
+//   std::cerr << "Saved in ASCII Fromat ." << std::endl;
+
+
+	// Plotter object.
+	pcl::visualization::PCLHistogramVisualizer viewer;
+	// We need to set the size of the descriptor beforehand.
+  viewer.addFeatureHistogram(*vfhFeatures, 308);
+  // viewer.addPointCloudNormals<pcl::PointXYZ,pcl::Normal>(vfhFeatures, normals);
+
+	viewer.spin();
+
+  return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,181 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/vfh.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/visualization/histogram_visualizer.h>
 
-int main(int argc, char** argv)
+// argument 1 => file ex : scene_mug_table.pcd 
+
+int 
+main (int argc, char** argv)
 {
-    std::cout << "aas sd" << std::endl;
-    std::cout << argv[1] << std::endl;
+    // Read in the cloud data
+    pcl::PCDReader reader;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>), cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
+    reader.read (argv[1], *cloud);
+    std::cout << "PointCloud before filtering has: " << cloud->size () << " data points." << std::endl; //*
+
+    // Create the filtering object: downsample the dataset using a leaf size of 1cm
+    pcl::VoxelGrid<pcl::PointXYZ> vg;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+    vg.setInputCloud (cloud);
+    vg.setLeafSize (0.01f, 0.01f, 0.01f);
+    vg.filter (*cloud_filtered);
+    std::cout << "PointCloud after filtering has: " << cloud_filtered->size ()  << " data points." << std::endl; //*
+
+    // Create the segmentation object for the planar model and set all the parameters
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZ> ());
+    pcl::PCDWriter writer;
+    seg.setOptimizeCoefficients (true);
+    seg.setModelType (pcl::SACMODEL_PLANE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setMaxIterations (100);
+    seg.setDistanceThreshold (0.02);
+
+    int i=0, nr_points = (int) cloud_filtered->size ();
+    while (cloud_filtered->size () > 0.3 * nr_points)
+    {
+        // Segment the largest planar component from the remaining cloud
+        seg.setInputCloud (cloud_filtered);
+        seg.segment (*inliers, *coefficients);
+        if (inliers->indices.size () == 0)
+        {
+            std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
+            break;
+        }
+
+        // Extract the planar inliers from the input cloud
+        pcl::ExtractIndices<pcl::PointXYZ> extract;
+        extract.setInputCloud (cloud_filtered);
+        extract.setIndices (inliers);
+        extract.setNegative (false);
+
+        // Get the points associated with the planar surface
+        extract.filter (*cloud_plane);
+        std::cout << "PointCloud representing the planar component: " << cloud_plane->size () << " data points." << std::endl;
+
+        // Remove the planar inliers, extract the rest
+        extract.setNegative (true);
+        extract.filter (*cloud_f);
+        *cloud_filtered = *cloud_f;
+    }
+
+    // Creating the KdTree object for the search method of the extraction
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+    tree->setInputCloud (cloud_filtered);
+
+    std::vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+    ec.setClusterTolerance (0.02); // 2cm
+    ec.setMinClusterSize (100);
+    ec.setMaxClusterSize (25000);
+    ec.setSearchMethod (tree);
+    ec.setInputCloud (cloud_filtered);
+    ec.extract (cluster_indices);
+
+    //// get the cluster models
+    //   int j = 0;
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> models;
+    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+    {
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
+        for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
+            cloud_cluster->push_back ((*cloud_filtered)[*pit]); //*
+        cloud_cluster->width = cloud_cluster->size ();
+        cloud_cluster->height = 1;
+        cloud_cluster->is_dense = true;
+
+        std::cout << "PointCloud representing the Cluster: " << cloud_cluster->size () << " data points." << std::endl;
+
+        models.push_back (cloud_cluster);
+        // std::stringstream ss;
+        // ss << "src/object_recognition_pkg/output/cloud_cluster_" << j << ".pcd";
+        // writer.write<pcl::PointXYZ> (ss.str (), *cloud_cluster, false); //*
+        // j++;
+    }
+
+
+    //// VFH DESCRIPTOR
+    // std::vector<pcl::PointCloud<pcl::VFHSignature308>::Ptr> modelsVFH;
+    std::vector<std::vector<float> > VFHValues;
     
+        pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normalEstimation;
+        typedef pcl::VFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::VFHSignature308> VFHEstimationType;
+        VFHEstimationType vfhEstimation;
+        pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+        pcl::PointCloud<pcl::Normal>::Ptr cloudWithNormals (new pcl::PointCloud<pcl::Normal>);
+        pcl::PointCloud<pcl::VFHSignature308>::Ptr vfhFeatures(new pcl::PointCloud<pcl::VFHSignature308>);
+
+    for(std::size_t i = 0; i < models.size(); ++i )
+    {
+        //  Compute the normals
+        normalEstimation.setInputCloud (cloud);
+
+        normalEstimation.setSearchMethod (tree);
+
+
+        normalEstimation.setRadiusSearch (0.03);
+
+        normalEstimation.compute (*cloudWithNormals);
+
+        std::cout << "Computed " << cloudWithNormals->points.size() << " normals." << std::endl;
+        
+        // Setup the feature computation
+
+        // Provide the original point cloud (without normals)
+        vfhEstimation.setInputCloud (cloud);
+
+        // Provide the point cloud with normals
+        vfhEstimation.setInputNormals(cloudWithNormals);
+
+        // Use the same KdTree from the normal estimation
+        vfhEstimation.setSearchMethod (tree);
+
+        //vfhEstimation.setRadiusSearch (0.2); // With this, error: "Both radius (.2) and K (1) defined! Set one of them to zero first and then re-run compute()"
+
+        // Actually compute the VFH features
+        vfhEstimation.compute (*vfhFeatures);
+
+        std::cout << "output points.size (): " << vfhFeatures->points.size () << std::endl; // This outputs 1 - should be 397!
+
+        // Display and retrieve the shape context descriptor vector for the 0th point.
+        // pcl::VFHSignature308 descriptor = vfhFeatures->points[0];
+        // VFHEstimationType::PointCloudOut::PointType descriptor2 = vfhFeatures->points[0];
+        // std::cout << descriptor << std::endl;
+
+        //push to vector 
+        // modelsVFH.push_back (vfhFeatures);
+
+        // VFHValues.push_back(vfhFeatures->points[0].histogram);
+        // std::vector<float> VFHValues;
+        // float vfh[308] = vfhFeatures->points[0].histogram;
+
+        std::vector<float> VFHValue;
+        for(auto it : vfhFeatures->points[0].histogram)
+        {
+            // std::cout << it << std::endl;
+            VFHValue.push_back(it);
+        }
+        VFHValues.push_back(VFHValue);
+    }
+    
+    std::cout << "jumlah vfh : " << VFHValues.size();
+
+    
+
+    return (0);
 }
